13d3b0fbf0d79054352242647277e32a9eac6734
Santosh <santosh@rumsan.com>
2023-02-03 15:10:53 +0545

    build path

--- src/contracts/projectContracts/UbiFactory.sol
+++ src/contracts/projectContracts/UbiFactory.sol
@@ -2,20 +2,16 @@
 pragma solidity ^0.8.17;
 
 //RahatProjectFactory
-interface IRahatProject {
-    ///@dev Accept Tokens from Rahatdonor
-    ///@dev Save the received token in a set.
-    ///@dev Save the no. of tokens issued to track total tokens received
-    function acceptToken(
-        address _token,
-        address _from,
-        uint256 _amount
-    ) external returns (bool);
+interface UbiFactory {
+  ///@dev Accept Tokens from Rahatdonor
+  ///@dev Save the received token in a set.
+  ///@dev Save the no. of tokens issued to track total tokens received
+  function acceptToken(address _token, address _from, uint256 _amount) external returns (bool);
 
-    ///@dev
-    function allocateTokensToBeneficiary(
-        address _rahatCommunity,
-        address _beneficiary,
-        uint256 _amount
-    ) external returns (bool);
+  ///@dev
+  function allocateTokensToBeneficiary(
+    address _rahatCommunity,
+    address _beneficiary,
+    uint256 _amount
+  ) external returns (bool);
 }
