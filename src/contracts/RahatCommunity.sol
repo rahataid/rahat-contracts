c75de06bf6acbed3222adaf1e8c5b3745ac6cd7f
anupamakoirala <anukoirala.96@hotmail.com>
2023-04-24 13:30:32 +0545

    Documentation added in rahat community contracts

--- src/contracts/RahatCommunity.sol
+++ src/contracts/RahatCommunity.sol
@@ -9,6 +9,10 @@ import '../interfaces/IRahatClaim.sol';
 import '../interfaces/IRahatProject.sol';
 import '../interfaces/IRahatCommunity.sol';
 
+/// @title Community contract to approve projects and beneficiaries
+/// @author Rumsan Associates
+/// @notice You can use this contract to manage Rahat projects and beneficiaries
+/// @dev Rahat Community supports UUPS Upgradeable contract
 contract RahatCommunity is Initializable, UUPSUpgradeable, IRahatCommunity, AccessControlUpgradeable, Multicall {
   // #region ***** Events *********//
   event ProjectApprovalRequest(address indexed requestor, address indexed project);
@@ -22,24 +26,37 @@ contract RahatCommunity is Initializable, UUPSUpgradeable, IRahatCommunity, Acce
   // #endregion
 
   // #region ***** Variables *********//
+  ///@notice name of the community
   string public name;
 
+  ///@notice map the beneficiary address with boolean
+  ///@dev checks if the address is beneficiary or not
   mapping(address => bool) public override isBeneficiary;
+
+  ///@notice map the project address with boolean
+  ///@dev checks if the project is approved or not
   mapping(address => bool) public override isProject;
 
+  ///@notice constant for vendor role
   bytes32 public constant VENDOR_ROLE = keccak256('VENDOR');
+
+  ///@notice constant for project interface id
   bytes4 public constant IID_RAHAT_PROJECT = type(IRahatProject).interfaceId;
 
   // #endregion
 
   // #region ***** Modifiers *********//
+  ///@notice Modifier to check if the caller is an admin
   modifier OnlyAdmin() {
     require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), 'Not an admin');
     _;
   }
 
   // #endregion
-
+  ///@notice Initialize the contract
+  ///@param _name Name of the community
+  ///@param _admin Address of the admin
+  ///@dev  called only once during the contract deployment
   function initialize(string memory _name, address _admin) public initializer {
     __AccessControl_init();
     _grantRole(DEFAULT_ADMIN_ROLE, _admin);
@@ -47,22 +64,24 @@ contract RahatCommunity is Initializable, UUPSUpgradeable, IRahatCommunity, Acce
     name = _name;
   }
 
-  // constructor(string memory _name, address _admin) {
-  //   _grantRole(DEFAULT_ADMIN_ROLE, _admin);
-  //   _setRoleAdmin(VENDOR_ROLE, DEFAULT_ADMIN_ROLE);
-  //   name = _name;
-  // }
-
   // #region ***** Role functions *********//
+  ///@notice checks if  a given address is admin or not
+  ///@param _address address to check
   function isAdmin(address _address) public view returns (bool) {
     return hasRole(DEFAULT_ADMIN_ROLE, _address);
   }
 
+  ///@notice add new beneficiary
+  ///@param _address address of the beneficiary
+  ///@dev only admin can call this function
   function addBeneficiary(address _address) public OnlyAdmin {
     isBeneficiary[_address] = true;
     emit BeneficiaryAdded(_address);
   }
 
+  ///@notice remove beneficiary
+  ///@param _address address of the beneficiary
+  ///@dev only admin can call this function
   function removeBeneficiary(address _address) public OnlyAdmin {
     isBeneficiary[_address] = false;
     emit BeneficiaryRemoved(_address);
@@ -72,6 +91,9 @@ contract RahatCommunity is Initializable, UUPSUpgradeable, IRahatCommunity, Acce
 
   // #region ***** Project functions *********//
 
+  ///@notice approve the project
+  ///@param _projectAddress address of the project
+  ///@dev only admin can call this function
   function approveProject(address _projectAddress) public OnlyAdmin {
     require(
       IERC165Upgradeable(_projectAddress).supportsInterface(IID_RAHAT_PROJECT),
@@ -81,15 +103,25 @@ contract RahatCommunity is Initializable, UUPSUpgradeable, IRahatCommunity, Acce
     isProject[_projectAddress] = true;
   }
 
+  ///@notice revvoke the project approval
+  ///@param _projectAddress address of the project
+  ///@dev only admin can call this function
   function revokeProject(address _projectAddress) public OnlyAdmin {
     if (isProject[_projectAddress]) emit ProjectRevoked(_projectAddress);
     isProject[_projectAddress] = false;
   }
 
+  ///@notice request project approval
+  ///@param _projectAddress address of the project
+  ///@dev any one can request for project approval
   function requestProjectApproval(address _projectAddress) public {
     emit ProjectApprovalRequest(tx.origin, _projectAddress);
   }
 
+  ///@notice grant role with eth
+  ///@param _role role to grant
+  ///@param _account account to grant role
+  ///@dev only admin can call this function
   function grantRoleWithEth(bytes32 _role, address _account) public OnlyAdmin {
     super.grantRole(_role, _account);
     if (_account.balance < 0.03 ether) {
@@ -98,15 +130,20 @@ contract RahatCommunity is Initializable, UUPSUpgradeable, IRahatCommunity, Acce
     }
   }
 
+  ///@notice fallback function to recieve ether
   receive() external payable {
     emit Received(msg.sender, msg.value);
   }
 
+  ///@notice withdraw ether from the contract
+  ///@param _to address to send ether
+  ///@dev only admin can call this function
   function withdraw(address payable _to) public payable OnlyAdmin {
     (bool sent, ) = _to.call{ value: address(this).balance }('');
     require(sent, 'Failed to send Ether');
   }
-
+  ///@notice authorize the upgrade 
+  ///@dev only admin can call this function and overide the function from UUPSUpgradeable
   function _authorizeUpgrade(address) internal override OnlyAdmin {}
 
   // #endregion
--- src/contracts/RahatCommunity1.sol
+++ src/contracts/RahatCommunity1.sol
@@ -5,7 +5,10 @@ pragma solidity ^0.8.17;
 import './RahatCommunity.sol';
 
 contract RahatCommunity1 is RahatCommunity{
-
+    
+    ///@notice change the name of the community
+    ///@param _name new name of the community
+    ///@dev only admin can call 
     function changeName(string memory _name) public OnlyAdmin {
         name = _name;
     }
