028fb226a479b352a152910e4aeb5d6046899374
myanzik <myanz.470shrestha@gmail.com>
2024-02-13 07:53:11 +0545

    added forwarder contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: {
-    version: '0.8.17',
+    version: '0.8.20',
     settings: {
       optimizer: {
         enabled: true,
